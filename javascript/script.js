905f31b627ae07f35508a797ce0c62c4c4778067
Gabriel Stanev <47984623+Shroom88@users.noreply.github.com>
2022-04-06 16:00:21 +0300

    contacts page with form added

--- javascript/script.js
+++ javascript/script.js
@@ -3,3 +3,12 @@ function progress(){
 	console.log("Hello World.");
 	// Ello
 }
+
+// Contacts Submit Form
+document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+	alert("Form sent!");
+  });
